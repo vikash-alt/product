880ee6f290f43e9f00097ca0eb411bba2b88cfc0
Vikash Kumar Mehta <104666707+vikash-alt@users.noreply.github.com>
2025-08-28 20:44:06 +0530

    Update Example.java

--- src/main/java/com/product/entity/Example.java
+++ src/main/java/com/product/entity/Example.java
@@ -1,8 +1,7 @@
 package com.product.entity;
 
 public class Example {
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
-
